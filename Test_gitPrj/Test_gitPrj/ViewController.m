a63103b56d3e47caf5912a64a73ee0fad414c258
FJL <776476088@qq.com>
2016-02-24 14:27:39 +0800

    更改背景颜色

--- Test_gitPrj/Test_gitPrj/ViewController.m
+++ Test_gitPrj/Test_gitPrj/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     self.title = @"github 代码修改";
     // Do any additional setup after loading the view, typically from a nib.
+    //尝试一下
+    self.view.backgroundColor = [UIColor yellowColor];
 }
 
 - (void)didReceiveMemoryWarning {
